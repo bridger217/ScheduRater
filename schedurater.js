1255565e50125a6624fed936faf4e8cfc1e157c2
Josh Algranati <jalgrana@umich.edu>
2019-10-30 19:04:48 -0400

    lets get alert working

--- schedurater.js
+++ schedurater.js
@@ -1,11 +1,21 @@
 //schedurater.js
 // jalgrana-mitchhub-elbridge
-var container_class = "row week-spanning row-no-padding row-no-margin";
-
-var container = document.getElementsByClassName(container_class);
-
+alert("MitchHub");
+console.log("Mitch sucks eggs");
+// var container_class = "row week-spanning row-no-padding row-no-margin";
 //
-if (container != null){
-  var classes = [];
-
-}
+// var container = document.getElementsByClassName(container_class);
+//
+// container[]
+//
+//
+// var profs = new Set();
+// if (container != null){
+//   for (var i = 1; i < container.length; i++){
+//     for (var j = 0; j < container[i].length; j++){
+//       profs.add(container[i][j][1][1][1][0].html())
+//     }
+//   }
+// }
+//
+// alert("MitchHub")
