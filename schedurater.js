317e8aba4a6445435cf03a3701fc5142b74dc3b2
Josh Algranati <jalgrana@umich.edu>
2019-10-30 18:12:19 -0400

    schedurater.js

---
+++ schedurater.js
@@ -0,0 +1,11 @@
+//schedurater.js
+// jalgrana-mitchhub-elbridge
+var container_class = "row week-spanning row-no-padding row-no-margin";
+
+var container = document.getElementsByClassName(container_class);
+
+//
+if (container != null){
+  var classes = [];
+
+}
