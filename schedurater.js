8b1a87acf4c7d90a63b3b4f678a5590d670cc8f7
Josh Algranati <jalgrana@umich.edu>
2019-10-30 21:55:21 -0400

    fixed small bug

--- schedurater.js
+++ schedurater.js
@@ -24,9 +24,17 @@ async function correctURL(){
   if (container != null){
     for (var i = 1; i < container[0].children.length; i++){
       for (var j = 0; j < container[0].children[i].children.length; j++){
-        var prof = container[0].children[i].children[j].children[1].children[1].children[2].children[0].children[1].innerHTML;
-        prof = prof.replace(/\s+/g, '');
-        profs.add(prof);
+        var child = container[0].children[i].children[j].children[1].children[1];
+        //they were assholes and put child nodes in different locations in the DOM
+        for (var c = 0; c < child.children.length; c++){
+          if (child.children[c].className === "meeting-text"){
+            console.log(c)
+            break
+          }
+        }
+        var prof = child.children[c].children[0].children[1].innerHTML;
+
+        profs.add(prof.trim());
       }
 
     }
