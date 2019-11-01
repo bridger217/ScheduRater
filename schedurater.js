756608b77f08fcd896fda999b73ed8c2e8d82833
Josh Algranati <jalgrana@umich.edu>
2019-11-01 14:09:02 -0400

    hover over works now

--- schedurater.js
+++ schedurater.js
@@ -11,13 +11,12 @@ function sleep(ms) {
 // }
 
 function setDisplayVisible(id){
-  console.log(id);
   document.getElementById(id).style.display = "block";
 }
 
 function setDisplayHidden(id){
   console.log(id);
-  document.getElementById(id).style.display = "hidden";
+  document.getElementById(id).style.display = "none";
 }
 
 
@@ -28,29 +27,30 @@ function setDisplayHidden(id){
 async function correctURL(){
   while(!document.URL.includes("schedules/")){
     console.log(window.location.href);
-    await sleep(5000);
+    await sleep(1000);
   }
 
-  var container_class = "row week-spanning row-no-padding row-no-margin";
+  let container_class = "row week-spanning row-no-padding row-no-margin";
 
-  var container = document.getElementsByClassName(container_class);
+  let container = document.getElementsByClassName(container_class);
 
-  var profs = new Set();
+  let profs = new Set();
 
 
   if (container != null){
-    for (var i = 1; i < container[0].children.length; i++){
-      for (var j = 0; j < container[0].children[i].children.length; j++){
-        var child = container[0].children[i].children[j].children[1].children[1];
-        for (var c = 0; c < child.children.length; c++){
+    for (let i = 1; i < container[0].children.length; i++){
+      for (let j = 0; j < container[0].children[i].children.length; j++){
+        let child = container[0].children[i].children[j].children[1].children[1];
+        let c = 0;
+        for (c = 0; c < child.children.length; c++){
           if (child.children[c].className === "meeting-text"){
             break
           }
         }
-        var prof = child.children[c].children[0].children[1].innerText;
+        let prof = child.children[c].children[0].children[1].innerText;
 
-        var newDiv = document.createElement("div");
-        var id  = i.toString() + j.toString();
+        let newDiv = document.createElement("div");
+        let id  = i.toString() + j.toString();
 
         newDiv.id = id;
         newDiv.style.width = "100px";
@@ -58,12 +58,12 @@ async function correctURL(){
         newDiv.style.background = "red";
         newDiv.style.display = "none";
         container[0].children[i].children[j].appendChild(newDiv);
-        container[0].children[i].children[j].addEventListener("mouseover", function(){
-          newDiv.style.display = "block";
-        });
-        container[0].children[i].children[j].addEventListener("mouseout", function(){
-          newDiv.style.display = "none";
-        });
+        container[0].children[i].children[j].addEventListener("mouseenter", () => {
+          setDisplayVisible(id);
+        },false);
+        container[0].children[i].children[j].addEventListener("mouseleave", () => {
+          setDisplayHidden(id);
+        },false);
         profs.add(prof.trim());
       }
     }
@@ -72,11 +72,11 @@ async function correctURL(){
 
   const Http = new XMLHttpRequest();
 
-  var arr_profs = Array.from(profs);
+  let arr_profs = Array.from(profs);
   console.log(arr_profs);
   console.log(arr_profs.length);
-  for (var p = 0; p < arr_profs.length; p++){
-    var search = "https://www.ratemyprofessors.com/search.jsp?query=";
+  for (let p = 0; p < arr_profs.length; p++){
+    let search = "https://www.ratemyprofessors.com/search.jsp?query=";
     search += arr_profs[p].replace(" ","+");
     console.log(search);
     Http.open("GET", search);
@@ -87,7 +87,7 @@ async function correctURL(){
     }
   }
 
-  var ifrm = document.createElement('iframe');
+  let ifrm = document.createElement('iframe');
   ifrm.setAttribute('id', 'ifrm');
   ifrm.src = chrome.runtime.getURL('iframe.html');
   document.body.appendChild(ifrm);
